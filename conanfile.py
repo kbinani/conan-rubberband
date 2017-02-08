d3b863f779dab9b2a78d4d235b70897e8c72e780
kbinani <kbinani.bt@gmail.com>
2017-02-08 13:46:41 +0900

    Separate packages for Debug and Release on Windows

--- conanfile.py
+++ conanfile.py
@@ -21,29 +21,14 @@ class RubberbandConan(ConanFile):
 
     def build(self):
         static = "-DBUILD_SHARED_LIBS=ON" if self.options.shared else ""
-        # Build both Debug and Release in one package id for Windows.
-        build_types = ["Debug", "Release"] if self.settings.os == "Windows" else [self.settings.build_type]
-        for build_type in build_types:
-            settings = self.settings
-            settings.build_type = build_type
-            cmake = CMake(settings)
-            self.run("cmake . %s %s" % (cmake.command_line, static))
-            self.run("cmake --build . %s" % cmake.build_config)
+        cmake = CMake(self.settings)
+        self.run("cmake . %s %s" % (cmake.command_line, static))
+        self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
         self.copy("*.h", src="rubberband", dst="include/rubberband", keep_path=False)
-        if self.settings.os == "Windows":
-            for build_type in ["Debug", "Release"]:
-                self.copy("*.dll", src=build_type, dst="lib/%s" % build_type, keep_path=False)
-                self.copy("*.lib", src=build_type, dst="lib/%s" % build_type, keep_path=False)
-        else:
-            self.copy("*.a", src=".", dst="lib", keep_path=False)
-            self.copy("*.dylib", src=".", dst="lib", keep_path=False)
+        for ext in ["dll", "lib", "a", "dylib"]:
+            self.copy("*.%s" % ext, src=".", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["rubberband"]
-
-    def conan_info(self):
-        if self.settings.os == "Windows":
-            # This will make package id same for both "Debug" and "Release".
-            self.info.settings.build_type = "DebugAndRelease"
